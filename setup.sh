27010dae5e42e2f8e0909716e596c4ce990ae242
Bastian Klein <basklein@amazon.com>
2021-12-13 15:44:06 +0100

    Use AWS_REGION env variable as priority before falling back to profile configuration

--- setup.sh
+++ setup.sh
@@ -125,8 +125,15 @@ then
   
   IMAGES=( BBBgreenlightImage BBBScaleliteNginxImage BBBScaleliteApiImage BBBScalelitePollerImage BBBScaleliteImporterImage )
 
+  if [[ -z "${AWS_REGION}" ]]; then
+    REGION=$(aws configure get region --profile=$BBBPROFILE)
+    echo "Using default region ${REGION} from profile ${BBBPROFILE}"
+  else
+    REGION=${AWS_REGION}
+    echo "Using region ${REGION} from AWS_REGION environment variable"
+  fi
+
   ACCOUNTID=$(aws sts get-caller-identity --query Account --output text --profile=$BBBPROFILE)
-  REGION=$(aws configure get region --profile=$BBBPROFILE)
   REGISTRY=$ACCOUNTID.dkr.ecr.$REGION.amazonaws.com
   SCALEILITEREGISTRY=$ACCOUNTID.dkr.ecr.$REGION.amazonaws.com/$SCALEILITEREGISTRY  
   GREENLIGHTREGISTRY=$ACCOUNTID.dkr.ecr.$REGION.amazonaws.com/$GREENLIGHTREGISTRY  
