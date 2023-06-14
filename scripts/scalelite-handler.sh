b0899606c2a9b22af17931bc8c4fdfedd876c657
David Surey <suredavi@amazon.de>
2023-06-14 23:33:12 +0200

    removal of add/remove app server scalelite-handler task, scalelite-handler.sh is now executing directly against the api task

--- scripts/scalelite-handler.sh
+++ scripts/scalelite-handler.sh
@@ -2,7 +2,7 @@
 # Script handles the add and remove for Application Server instances into scalelite
 # only for testing purposes
 
-while getopts ":s:p:m:r:c:n:u:g:" opt; do
+while getopts ":s:p:m:r:c:" opt; do
   case $opt in
     p) SECRET="$OPTARG"
     ;;
@@ -12,13 +12,7 @@ while getopts ":s:p:m:r:c:n:u:g:" opt; do
     ;;
     r) REGION="$OPTARG"
     ;;
-    c) ECSCLUSTER="$OPTARG"
-    ;;
-    n) ECSMODE="$OPTARG"
-    ;;
-    u) TASK_SUBNETS="$OPTARG"
-    ;;
-    g) TASK_SGS="$OPTARG"
+    c) ECS_CLUSTER="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
@@ -28,24 +22,20 @@ done
 OIFS=$IFS;
 IFS=",";
 
-TASKSUBS=($TASK_SUBNETS)
+# get the Scalelite Information
+SCALELITE_SERVICE=$(aws ecs list-services --region $REGION --cluster $ECS_CLUSTER --query "serviceArns[?contains(@, 'BBBScaleliteService')]" --output text | xargs -n 1 basename)
+SCALELITE_TASK=$(aws ecs list-tasks --region $REGION --cluster $ECS_CLUSTER --service $SCALELITE_SERVICE --output text | awk -F"/" '{print $NF}' | rev | awk -F"/" '{print $1}' | rev)
 
-TASK_ARN=$(aws ecs list-task-definitions --region $REGION | jq -r '[ .taskDefinitionArns[] | select(contains("scalelite-handle-server")) ] | last')
-CLUSTER=$(aws ecs list-clusters --region $REGION | jq -r ".clusterArns[] | select(contains(\"$ECSCLUSTER\"))" )
-
+# Decide if to add or to remove
 if [[ $METHOD == "create" ]]; then
   COMMAND_STRING="id=\$(bin/rake servers:add[$SERVER,$SECRET] | tail -n 1 | sed 's/id: //g'); bin/rake servers:enable[\$id];"
 fi
 
 if [[ $METHOD == "delete" ]]; then
-  COMMAND_STRING="id=\$(./bin/rake servers | grep -B 1 "$SERVER" | head -n 1 | sed 's/id: //g'); bin/rake servers:panic[\$id] && bin/rake servers:remove[\$id]"
+  COMMAND_STRING="id=\$(bin/rake servers | grep -B 1 "$SERVER" | head -n 1 | sed 's/id: //g'); bin/rake servers:panic[\$id] && bin/rake servers:remove[\$id]"
 fi
 
-
-if [[ $ECSMODE == "fargate" ]]; then
-  aws ecs run-task --task-definition "$TASK_ARN" --cluster "$CLUSTER" --region $REGION --launch-type "FARGATE" --network-configuration "{\"awsvpcConfiguration\": {\"subnets\": [\"$TASKSUBS\"],\"securityGroups\": [\"$TASK_SGS\"],\"assignPublicIp\": \"DISABLED\"}}" --overrides "{\"containerOverrides\": [{\"name\": \"scalelite-handle-server\",\"command\": [\"/bin/sh\", \"-c\", \"$COMMAND_STRING\"]}]}"
-else
-  aws ecs run-task --task-definition "$TASK_ARN" --cluster "$CLUSTER" --region $REGION --overrides "{\"containerOverrides\": [{\"name\": \"scalelite-handle-server\",\"command\": [\"/bin/sh\", \"-c\", \"$COMMAND_STRING\"]}]}"
-fi
+# execute the needed command at the api container
+aws ecs execute-command --region $REGION --cluster "$ECS_CLUSTER" --task $SCALELITE_TASK --container scalelite-api --interactive --command "/bin/sh -c \"$COMMAND_STRING\""
 
 IFS=$OIFS;
